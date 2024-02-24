1ebbcd89dbb51e9ce96f108cca13e99136bc03e8
Shreevathsav <58297203+Shreevathsav@users.noreply.github.com>
2024-02-24 14:30:15 +0530

    added angular login page

---
+++ Angular-xerox-app/xerox-app-admin/src/app/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+  encapsulation: ViewEncapsulation.None
+})
+export class LoginComponent {
+
+}
